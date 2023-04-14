adf2cf52cf9f3085a48b9b7e56b075cee3152b98
DanielCantabella <daniel.cantabella.cantabella@estudiantat.upc.edu>
2023-04-15 00:08:48 +0200

    upload in memory done

--- src/writer/avroWriter.py
+++ src/writer/avroWriter.py
@@ -73,14 +73,10 @@ def api2avro(json_list, schemaName):
     # Return the Avro content as bytes
     return avro_output_file_content
 
-
-
-
-
-def file2avro(inputArg, schemaName, rawDataFolderName, outputFolderName, outputFile=""):
+def file2avro(inputArg, schemaName, rawDataFolderName, outputFolderName):
     schemaFile = PROJECT_DIRECTORY + "/resources/" + schemaName + ".avsc"
     dataFolder = PROJECT_DIRECTORY + "/data/" + rawDataFolderName
-    avroOutputFilePath = PROJECT_DIRECTORY + "/outputFiles/avroFiles/" + outputFolderName + outputFile
+
     if not os.path.exists(dataFolder):
         raise Exception(f"The directory {dataFolder} does not exist. Please create it.")
     if not os.path.exists(os.path.dirname(schemaFile)):
@@ -88,21 +84,71 @@ def file2avro(inputArg, schemaName, rawDataFolderName, outputFolderName, outputF
     else:
         schema = avro.schema.parse(open(schemaFile, "rb").read())
 
-    os.makedirs(os.path.dirname(avroOutputFilePath), exist_ok=True)
 
     for filename in os.listdir(dataFolder):
         file = os.path.join(dataFolder, filename)
-        writer = DataFileWriter(open(avroOutputFilePath + str(filename.split('.')[0]) + ".avro", "wb"), DatumWriter(), schema)
+        # writer = DataFileWriter(open(avroOutputFilePath + str(filename.split('.')[0]) + ".avro", "wb"), DatumWriter(), schema)
         if os.path.isfile(file):
             with open(file, 'r') as dataFile:
+                # Create an in-memory file object
+                avro_output_file = io.BytesIO()
+                # Create an Avro DatumWriter
+                datum_writer = avro.io.DatumWriter(schema)
+                # Create an Avro BinaryEncoder
+                avro_encoder = avro.io.BinaryEncoder(avro_output_file)
                 if inputArg == "property":
-                    data = json.load(dataFile)
+                    json_data = json.load(dataFile)
+                    for json_obj in json_data:
+                        datum_writer.write(json_obj, avro_encoder)
+                    avro_output_file_content = avro_output_file.getvalue()
+                    outputHDFSfolderName = HDFS_DIRECTORY + "avroFiles/" + outputFolderName + "idealista$" + "json$" + filename.split(".")[0] + ".avro"
+                    delete_hdfs_folder(outputHDFSfolderName)  # Allows to overwrite the files in HDFS. Comment if you don't want to overwrite.
+                    upload_memory_to_hdfs(avro_output_file_content, outputHDFSfolderName)
+
                 elif inputArg == "income" or inputArg == "lookup":
-                    data = csv.DictReader(dataFile)
-                for record in data:
-                    writer.append(record)
-        writer.close()
-    return avroOutputFilePath
+                    csv_reader = csv.DictReader(dataFile)
+                    avro_output_file = io.BytesIO()
+                    datum_writer = avro.io.DatumWriter(schema)
+                    avro_encoder = avro.io.BinaryEncoder(avro_output_file)
+                    for row in csv_reader:
+                        datum_writer.write(row, avro_encoder)
+                    avro_output_file_content = avro_output_file.getvalue()
+                    if inputArg == "income":
+                        outputHDFSfolderName = HDFS_DIRECTORY + "avroFiles/" + outputFolderName + "opendatabcn-income$" + "csv$" + filename.split(".")[0] + ".avro"
+                    else:
+                        outputHDFSfolderName = HDFS_DIRECTORY + "avroFiles/" + outputFolderName + "lookup_tables$" + "csv$" + filename.split(".")[0] + ".avro"
+                    delete_hdfs_folder(outputHDFSfolderName)  # Allows to overwrite the files in HDFS. Comment if you don't want to overwrite.
+                    upload_memory_to_hdfs(avro_output_file_content, outputHDFSfolderName)
+
+
+
+
+# def file2avro(inputArg, schemaName, rawDataFolderName, outputFolderName, outputFile=""):
+#     schemaFile = PROJECT_DIRECTORY + "/resources/" + schemaName + ".avsc"
+#     dataFolder = PROJECT_DIRECTORY + "/data/" + rawDataFolderName
+#     avroOutputFilePath = PROJECT_DIRECTORY + "/outputFiles/avroFiles/" + outputFolderName + outputFile
+#     if not os.path.exists(dataFolder):
+#         raise Exception(f"The directory {dataFolder} does not exist. Please create it.")
+#     if not os.path.exists(os.path.dirname(schemaFile)):
+#         raise Exception(f"The directory {os.path.dirname(schemaFile)} does not exist. Please create it.")
+#     else:
+#         schema = avro.schema.parse(open(schemaFile, "rb").read())
+#
+#     os.makedirs(os.path.dirname(avroOutputFilePath), exist_ok=True)
+#
+#     for filename in os.listdir(dataFolder):
+#         file = os.path.join(dataFolder, filename)
+#         writer = DataFileWriter(open(avroOutputFilePath + str(filename.split('.')[0]) + ".avro", "wb"), DatumWriter(), schema)
+#         if os.path.isfile(file):
+#             with open(file, 'r') as dataFile:
+#                 if inputArg == "property":
+#                     data = json.load(dataFile)
+#                 elif inputArg == "income" or inputArg == "lookup":
+#                     data = csv.DictReader(dataFile)
+#                 for record in data:
+#                     writer.append(record)
+#         writer.close()
+#     return avroOutputFilePath
 
 def writeAvro(inputArg):
 
@@ -129,22 +175,23 @@ def writeAvro(inputArg):
                 continue
             # avroLocalOutputFilePath = api2avro(apiData, schemaName, outputFolderName)
             memoryFile = api2avro(apiData, schemaName)
-            delete_hdfs_folder(HDFS_DIRECTORY+"avroFiles/"+outputFolderName + filenames[index]) # Allows to overwrite the files in HDFS. Comment if you don't want to overwrite.
-            upload_memory_to_hdfs(memoryFile, HDFS_DIRECTORY+"avroFiles/"+outputFolderName + filenames[index] + ".avro")
+            outputHDFSfolderName = HDFS_DIRECTORY+"avroFiles/"+outputFolderName + "opendatabcn-immigration$" + "json$" + filenames[index]+ ".avro"
+            delete_hdfs_folder(outputHDFSfolderName) # Allows to overwrite the files in HDFS. Comment if you don't want to overwrite.
+            upload_memory_to_hdfs(memoryFile, outputHDFSfolderName)
 
 
     if schemaName is None:
         raise ValueError("Invalid inputArg provided")
 
     if inputArg != "immigration":
-        avroLocalOutputFilePath = file2avro(inputArg, schemaName, rawDataFolderName, outputFolderName)
-    # delete_hdfs_folder(HDFS_DIRECTORY+"avroFiles/"+outputFolderName) # Allows to overwrite the files in HDFS. Comment if you don't want to overwrite.
-    # upload_folder_to_hdfs(avroLocalOutputFilePath, HDFS_DIRECTORY+"avroFiles/"+outputFolderName)
+        file2avro(inputArg, schemaName, rawDataFolderName, outputFolderName)
+
 
 
 if __name__ == '__main__':
-    writeAvro("immigration")
+    # writeAvro("immigration")
     # writeAvro("property")
-    # writeAvro("income")
-    # writeAvro("lookup")
+    writeAvro("income")
+    writeAvro("lookup")
     # json_to_avro()
+
